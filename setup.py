a17beea57e36274c105576cfda5a01fd834b8a57
Christophe Eymard <christophe@ravelsoft.com>
2012-01-19 19:16:05 +0100

    Added missing files.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pwilang',
+      version='0.11.0',
+      description='Python pwilang parser',
+      author='Christophe Eymard',
+      author_email='christophe.eymard@ravelsoft.com',
+      url='http://pwilang.github.com/',
+      packages=['pwilang'],
+      scripts=['scripts/pwilang']
+     )
