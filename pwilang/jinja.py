78fa991f80ca0824b7dba2fabc754c0733e716fb
Christophe Eymard <christophe@ravelsoft.com>
2012-01-19 18:10:52 +0100

    Moving to nicer structure.
    --HG--
    rename : nodes.py => pwilang/nodes.py
    rename : printer.py => pwilang/printer.py
    rename : pwilang.pwpeg => pwilang/pwilang.pwpeg
    rename : pwilang.py => pwilang/pwilang.py
    rename : test => scripts/pwilang

---
+++ pwilang/jinja.py
@@ -0,0 +1,35 @@
+from jinja2 import  Environment, TemplateSyntaxError
+from jinja2.ext import Extension
+
+from pwpeg import Parser
+
+from . import pwilang
+from . import printer
+
+class PwilangExtension(Extension):
+
+    def __init__(self, environment):
+        super(PwilangExtension, self).__init__(environment)
+
+        environment.extend(
+            pwilang_compact=False,
+            pwilang_file_extensions=('.pwi',),
+        )
+
+        self.parser = Parser(pwilang.block)
+
+
+    def preprocess(self, source, name, filename=None):
+        if name is None or os.path.splitext(name)[1] not in \
+            self.environment.pwilang_file_extensions:
+            return source
+
+        visitor = printer.NodeVisitor(self.environment.pwilang_compact)
+
+        try:
+            root = self.parser.parse(source)
+            return visitor.visit(root)
+        except TemplateIndentationError, e:
+            raise TemplateSyntaxError(e.message, e.lineno, name=name, filename=filename)
+        except TemplateSyntaxError, e:
+            raise TemplateSyntaxError(e.message, e.lineno, name=name, filename=filename)
