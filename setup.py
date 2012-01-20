c2c12a8e229fddee81e13e2ab0f1504880602801
Christophe Eymard <christophe@ravelsoft.com>
2012-01-20 12:21:06 +0100

    Version bump 0.11.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pwilang',
-      version='0.11.0',
+      version='0.11.1',
       description='Python pwilang parser',
       author='Christophe Eymard',
       author_email='christophe.eymard@ravelsoft.com',
